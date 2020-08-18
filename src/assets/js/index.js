8e8794f2a25702be1ea9074a08fcf0baa175aa2c
songsong <506053687@qq.com>
2020-08-18 17:38:01 +0800

    1.0

---
+++ src/assets/js/index.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper('.swiper-containerA', {
+    pagination: '.swiper-paginationA',
+    paginationClickable: true
+});
+var swiper2 = new Swiper('.swiper-container', {
+    scrollbarHide: true,
+    slidesPerView: 'auto',
+    loop: true,
+    centeredSlides: false,
+    spaceBetween: 20,
+    grabCursor: true,
+    autoplay : 2000,
+});
